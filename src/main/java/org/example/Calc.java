cdd9714415f651d275a7b87bf7827e4c9403f685
philip <kansastowny@gmail.com>
2025-08-17 18:48:39 +1200

    Implement subtract; tests pass

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -2,13 +2,6 @@ package org.example;
 
 public class Calc {
 
-    public int add(int x, int y)
-    {
-        return x+y;
-    }
-
-    public int subtract(int x, int y)
-    {
-        return x*y;
-    }
+    public int add(int a, int b) { return a + b; }
+    public int subtract(int a, int b) { return a - b; }
 }
--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -4,17 +4,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class CalcTest {
-  Calc c = new Calc();
+    private final Calc c = new Calc();
 
     @Test
     void testAddition() {
-        assertEquals(4, c.add(2,2));
+        assertEquals(4, c.add(2, 2));
     }
 
     @Test
-public void testSubtraction() {
-    Calc c = new Calc();
-    assertEquals(2, c.subtract(4, 2));
-}
-
+    void testSubtraction() {
+        assertEquals(2, c.subtract(4, 2));
+    }
 }
